080b94d8cbca9c00a3b09f1e990ea3d0930afc1b
Tange <tantantatange@gmail.com>
2021-07-02 23:52:20 +0900

    Add sample Image

---
+++ Todo/CategoryImage.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryImage.swift
+//  Todo
+//
+//  Created by Atsuya Tange on 2021/07/02.
+//
+
+import SwiftUI
+
+struct CategoryImage: View {
+    var body: some View {
+        Image(systemName: "tortoise.fill")
+            .resizable()
+            .scaledToFit()
+            .foregroundColor(.white)
+            .padding(2.0)
+            .frame(width: 30, height: 30)
+            .background(Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)))
+            .cornerRadius(6.0)
+    }
+}
+
+struct CategoryImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryImage()
+    }
+}
