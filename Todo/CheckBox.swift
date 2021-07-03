734d6655851eb4d639834170d137e3b7d67ff41c
Tange <tantantatange@gmail.com>
2021-07-03 23:34:27 +0900

    changed CheckBox to generic structure

--- Todo/CheckBox.swift
+++ Todo/CheckBox.swift
@@ -7,19 +7,40 @@
 
 import SwiftUI
 
-struct CheckBox: View {
-    // SwiftUIで管理。変数の変更を自動的に画面に反映
-    @State var checked: Bool = false
+struct CheckBox<Label>: View where Label: View {
+    
+    // 他のViewから値を受け取る
+    @Binding var checked: Bool
+    
+    private var label: () -> Label
+    
+    public init(checked: Binding<Bool>, @ViewBuilder label: @escaping () -> Label) {
+        self._checked = checked
+        self.label = label
+    }
+    
+    
     var body: some View {
-        // バインディング。$をつけることで複数のViewで共有。
-        Toggle(isOn: $checked) {
-            Text("チェックボックス")
+        HStack {
+            Image(systemName: checked ? "checkmark.circle" : "circle")
+                .onTapGesture {
+                    self.checked.toggle()
+            }
+            label()
         }
     }
 }
 
 struct CheckBox_Previews: PreviewProvider {
     static var previews: some View {
-        CheckBox()
+        VStack {
+            CheckBox(checked: .constant(false)) {
+                Text("牛乳を買う")
+            }
+            CheckBox(checked: .constant(true)) {
+                Image(systemName: "hand.thumbsup")
+                Text("牛乳を買う")
+            }
+        }
     }
 }
